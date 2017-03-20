e419c3a55af89a1e3f4a641ccaefa5447f81f1e3
Kris Ekenes <krisekenes@gmail.com>
2017-03-20 11:58:49 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,79 +1,111 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, } from "react";
+import ReactDOM from "react-dom";
 
 const Header = (props) => {
-  return (
+    return (
     <header>
       <h1>Dojo Todos</h1>
       <h3>Todo list</h3>
     </header>
-  )
-}
+    );
+};
 
 const FilterPanel = (props) => {
-  return (
+    return (
     <div>
-      <button type="">All</button>
-      <button type="">Completed</button>
-      <button type="">Pending</button>
+      <button onClick={props.handleFilter} type="" value="all">All</button>
+      <button onClick={props.handleFilter} type="" value="completed">Completed</button>
+      <button onClick={props.handleFilter} type="" value="pending">Pending</button>
     </div>
-  )
-}
+    );
+};
 
 const List = (props) => {
-  return (
+    const listItemCollection = props.items.map((item, index) => <ListItem key={index} onChange={props.onChange} index={index} {...item} handleCheck={props.handleCheck} deleteTodo={props.deleteTodo} />);
+    return (
     <ul>
-      <ListItem text={"Don't be Len Bias"}/>
-      <ListItem text={"Try new things"}/>
-      <ListItem text={"Practice basetball"}/>
+      {listItemCollection}
     </ul>
-  )
-}
+    );
+};
 
-const ListItem = ({text}) => {
-  return (
-    <div>
-      <input type="checkbox" />{text} 
-      <button type="">Delete</button>
-    </div>
-  )
-}
-
-const TodoForm = (props) => {
-  return (
-    <form>
-      <input type="text" name=""  />
-      <input type="submit" name="" value="Add" />
-    </form>
-  )
-}
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      items: [
-        { text: "Learn React", completed: true },
-        { text: "Build a todo app", completed: false },
-        { text: "Profit!", completed: false }
-      ],
-      filter: 'all',
-      newTodoText: ''
-    }
-  }
-  render(){
+const ListItem = ({text ,completed, index, id, onChange, deleteTodo, }) => {
     return (
       <div>
-        <Header />
-        <FilterPanel />
-        <List />
-        <TodoForm />
+        <input type="checkbox" checked={completed} value={index} onChange={onChange} />{text} 
+        <button type="" onClick={e => deleteTodo(e, id)}>Delete</button>
       </div>
-    )
-  }
+    );
+};
+
+const TodoForm = ({createTodo,}) => {
+    let textInput = null;
+    return (
+    <form onSubmit={e => createTodo(e, textInput.value)}>
+      <input type="text" ref={(input) => { textInput = input; }} />
+      <input type="submit" value="Add" />
+    </form>
+    );
+};
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            items: [
+              { id: 1, text: "Learn React", completed: true, },
+              { id: 2, text: "Build a todo app", completed: false, },
+              { id: 3, text: "Profit!", completed: false, },
+            ],
+            filter: "all",
+            newTodoText: "",
+        };
+        this.handleFilter =  this.handleFilter.bind( this );
+        this.onChange =  this.onChange.bind( this );
+        this.createTodo = this.createTodo.bind( this );
+        this.deleteTodo = this.deleteTodo.bind( this );
+    }
+    handleFilter(e) {
+        this.setState({filter: e.target.value,});
+    }
+    onChange(e){
+        let items = this.state.items;
+        items[e.target.value].completed = !this.state.items[e.target.value].completed;
+        this.setState({ items: items, });
+    }
+    createTodo(e, text){
+        e.preventDefault();
+        let items = this.state.items;
+        let id = items.length ? items[items.length-1].id : 0;
+        items.push({ id:id+1 , text:text, completed:false, });
+        this.setState({ items: items, });
+    }
+    deleteTodo(e, id){
+        let items = this.state.items;
+        items = items.filter(item => item.id !== id); 
+        this.setState({ items: items, });
+    }
+    render(){
+        let items = this.state.items;
+        if(this.state.filter === "completed"){
+            items = items.filter(item =>
+            item.completed);
+        } else if(this.state.filter === "pending"){
+            items = items.filter(item =>
+            !item.completed);
+        }
+        return (
+          <div>
+            <Header />
+            <FilterPanel handleFilter={this.handleFilter} />
+            <List items={items} onChange={this.onChange} deleteTodo={this.deleteTodo} />
+            <TodoForm createTodo={this.createTodo} />
+          </div>
+        );
+    }
 }
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
